dc73affa9446501ce29467b98e9c6f7e5d8fb086
Marcus Prado <mgprado1@my.waketech.edu>
2026-02-16 08:01:44 -0500

    Add game check before playing audio

--- main.js
+++ main.js
@@ -127,7 +127,9 @@ document.addEventListener("keypress", function (event) {
                 minecraftAudio.pause();
                 pauseOverlay.style = "display: flex;";
             } else {
+                if (game) {
                 minecraftAudio.play();
+                }
                 pauseOverlay.style = "display: none;";
             }
             playing = !playing
